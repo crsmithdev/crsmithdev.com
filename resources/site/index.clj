740da7b2483eb701edf24dba77b2f890f493b932
Chris Smith <chris@cir.ca>
2013-08-19 08:30:41 -0700

    placeholder text replacement on index, projects listed

--- resources/site/index.clj
+++ resources/site/index.clj
@@ -3,17 +3,25 @@
 [:div
  [:div {:class "row"}
   [:div {:class "col-md-12"}
-   [:h1 "Stumptown narwhal blog aesthetic."]]]
+   [:h1 "[needs a title]"]]]
  [:div {:class "row"}
   [:div {:class "col-md-6"}
    [:p {:class "lead text-muted"}
-    "Etsy sartorial Carles, master cleanse selfies butcher leggings. DIY fanny pack lo-fi."]]]
+    "Writings on code, startups and living in San Francisco."]]]
  [:div {:class "row"}
   [:div {:class "col-md-12"}
-   [:p "Pickled aesthetic organic freegan. Irony yr Terry Richardson, synth Banksy sustainable pour-over +1. Marfa 8-bit post-ironic tousled banjo gluten-free. Small batch whatever ennui, 3 wolf moon bespoke selfies Cosby sweater gastropub Schlitz you probably haven't heard of them blue bottle cred. Dreamcatcher Bushwick mlkshk narwhal sustainable, 8-bit selvage fingerstache +1. Yr viral pickled sustainable, craft beer shabby chic gentrify sriracha ethnic butcher keytar Schlitz kitsch. Terry Richardson ethnic mixtape, Tonx master cleanse jean shorts blog butcher pickled kale chips sriracha +1 polaroid helvetica before they sold out."]]]
+   [:p
+    "Hi, I'm Chris, an engineer from the midwest now living and working in the heart of downtown San Francisco. "
+    "I spend my days hacking for " [:a {:href "http://cir.ca"} "Circa"] ", developing and scaling APIs and other backend systems. "
+    "In my own time, I work on many side " [:a {:href "/projects.html"} "projects"] ", study architecture, functional programming, "
+    "languages, concurrency and scalability. My current tools of choice are Python, Scala, and Clojure.  This is both a personal "
+    "portfolio as well as a " [:a {:href "/blog.html"} "blog"] " of some of the interesting and enlightening things I've discovered."]]]
+  [:p
+   "I'm on " [:a {:href "http://twitter.com/crsmithdev"} "twitter"] " and " [:a {:href "http://github.com/crsmithdev"} "github"]
+   ", and you can also contact me " [:a {:href "mailto:crsmithdev@gmail.com"} "directly"] "."]
  [:div {:class "row"}
   [:div {:class "col-md-6"}
-   [:h4 "Recent Posts"]
+   [:h4 "Recent Blog Posts"]
    (map #(let [f % url (static.core/post-url f)
                [metadata _] (static.io/read-doc f)
                date (static.core/parse-date
@@ -24,5 +32,5 @@
        [:div date]]])
       (take 8 (reverse (static.io/list-files :posts))))]
     [:div {:class "col-md-6"}
-    [:h4 "Recent Activity"]
+    [:h4 "Recent Activity on Github"]
     [:div {:class "gh-recent"}]]]]
--- resources/site/projects.clj
+++ resources/site/projects.clj
@@ -3,4 +3,24 @@
 [:div
  [:div {:class "row"}
   [:div {:class "col-md-12"}
-   [:h2 "Projects"]]]]
+   [:h2 "Projects"]
+   [:p "Some of the interesting side projects I've worked on recently."]
+   [:h3 "Arrow"]
+   [:p
+    "Arrow is a simple, lightweight Python library, inspired by " [:a {:href "https://github.com/timrwood/moment"} "moment.js"] " and "
+    [:a {:href "https://github.com/kennethreitz/requests"} "requests"] ", that provides a sensible, intelligent way of creating "
+    "manipulating, formatting and converting dates, times and timestamps."]
+   [:p [:a {:href "http://crsmithdev.com/arrow"}, "crsmithdev.com/arrow"]]
+   [:h3 "crsmithdev.com"]
+   [:p
+    "crsmithdev.com is built using Boostrap 3 and " [:a {:href "https://github.com/nakkaya/static"} "Static"] ", a static site "
+    "generator for Clojure."]
+   [:p [:a {:href "http://github.com/crsmithdev/crsmithdev.com"}, "github.com/crsmithdev/crsmithdev.com"]]
+   [:h3 "PEW"]
+   [:p "PEW (a Python Eve Wrapper) is a basic Python client for the EVE online API."]
+   [:p [:a {:href "http://github.com/crsmithdev/pew"}, "github.com/crsmithdev/pew"]]
+   [:h3 "Data Structure Visualizer"]
+   [:p
+    "The Data Structure Visualizer is an academic project, built with C# / WPF, built to assist in teaching programming students "
+    "about the abstractions and behaviors involved in common data structures and sorting algorithms."]
+   [:p [:a {:href "mailto:crsmithdev@gmail.com"}, "Contact me"] " for more information about this project."]]]]
