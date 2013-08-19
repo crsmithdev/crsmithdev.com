9e4f22aa880c2d83e5d329d353c0689932f7a662
Chris Smith <chris@cir.ca>
2013-08-18 21:16:42 -0700

    js cleanup, min-height for github activity

--- resources/templates/default.clj
+++ resources/templates/default.clj
@@ -47,15 +47,14 @@
         success: function (data) {
           var commits = _.filter(data.data, function(e) {return e.type == 'PushEvent'});
           _.each(_.first(commits, 5), function(r) {
-            var commit_message = r.payload.commits[0].message
+            var commit_message = r.payload.commits[0].message;
             var commit_url = 'https://github.com/' + r.repo.name + '/commit/' + r.payload.commits[0].sha;
             var repo_name = r.repo.name.split('/')[1];
-            var date = moment(r.created_at, 'YYYY-MM-DDThh:mm:ssZ')
-            str = '<div><div><a href=\"' + commit_url + '\">' + commit_message + '</a>'
-            str += ' <span class=\"text-muted\">' + repo_name + '</span></div>';
-            str += '<div>' + date.format('DD MMM YYYY') + '</div></div>'
-            div = $(str);
-            $('.gh-recent').append(div);
+            var date = moment(r.created_at, 'YYYY-MM-DDThh:mm:ssZ');
+            html = '<div><div><a href=\"' + commit_url + '\">' + commit_message + '</a>';
+            html += ' <span class=\"text-muted\">' + repo_name + '</span></div>';
+            html += '<div>' + date.format('DD MMM YYYY') + '</div></div>';
+            $('.gh-recent').append($(html));
           })
         }
       });
