a19f292152072f4abc52e6966730f466a669b3da
Chris Smith <chris@cir.ca>
2013-08-19 19:46:36 -0700

    github activity w/o underscore

--- resources/templates/default.clj
+++ resources/templates/default.clj
@@ -44,30 +44,37 @@
     [:script {:src "http://code.jquery.com/jquery.js"}]
     [:script {:src "/js/lib/bootstrap.min.js"}]
     [:script {:src "/js/lib/highlight.pack.js"}]
-    [:script {:src "//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.4/underscore-min.js"}]
     [:script {:src "/js/lib/moment.min.js"}]]]
     "<script type=\"text/javascript\">
     //<![CDATA[
+
       var ghRecents = $('.gh-recent');
 
       if (ghRecents.length > 0) {
+
         $.ajax({
           url: 'https://api.github.com/users/crsmithdev/events',
           dataType: 'jsonp',
           success: function (data) {
-            var commits = _.filter(data.data, function(e) {return e.type == 'PushEvent'});
-            _.each(_.first(commits, 5), function(r) {
-              var commit_message = r.payload.commits[0].message;
-              var commit_url = 'https://github.com/' + r.repo.name + '/commit/' + r.payload.commits[0].sha;
-              var repo_name = r.repo.name.split('/')[1];
-              var date = moment(r.created_at, 'YYYY-MM-DDThh:mm:ssZ');
-              html = '<div><div><a href=\"' + commit_url + '\">' + commit_message + '</a>';
-              html += ' <span class=\"text-muted\">' + repo_name + '</span></div>';
-              html += '<div>' + date.format('DD MMMM YYYY') + '</div></div>';
-              ghRecents.append($(html));
-            })
+            var shown = 0;
+
+            for (var i = 0; i < data.data.length && shown < 5; ++i) {
+              var r = data.data[i];
+
+              if (r.type == 'PushEvent') {
+                var commit_message = r.payload.commits[0].message;
+                var commit_url = 'https://github.com/' + r.repo.name + '/commit/' + r.payload.commits[0].sha;
+                var repo_name = r.repo.name.split('/')[1];
+                var date = moment(r.created_at, 'YYYY-MM-DDThh:mm:ssZ');
+                html = '<div><div><a href=\"' + commit_url + '\">' + commit_message + '</a>';
+                html += ' <span class=\"text-muted\">' + repo_name + '</span></div>';
+                html += '<div>' + date.format('DD MMMM YYYY') + '</div></div>';
+                ghRecents.append($(html));
+                ++shown;
+              }
+            }
           }
-        })
+        });
       }
     //]]>
     </script>
